aebf7f15ccc21534de2366854794fd31956b4f7d
German N Montenegro <gnm3000@gmail.com>
2022-10-15 12:03:52 -0300

    output

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,17 +11,10 @@ mkdir /root/.aws
 printf "[default]
 region=us-east-1
 output=json" | tee -a /root/.aws/config >/dev/null
-echo "first param $1 \n\n"
-echo "2d param $2 \n\n"
-echo "3d param $3 \n\n"
-
-
 printf "[default]
 aws_access_key_id=$1
 aws_secret_access_key=$2
 aws_session_token=$3" | tee -a /root/.aws/credentials >/dev/null
-echo "my file /root/.aws/credentials:"
-cat /root/.aws/credentials
 aws --version
 aws s3 ls
 #echo "Hello $1 $2 $3"
