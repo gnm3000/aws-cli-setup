099f4dad68896f836cc4941a5eeccfac88cfe348
German N Montenegro <gnm3000@gmail.com>
2022-10-15 11:33:47 -0300

    aws

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
-FROM python:3.8.2-alpine3.11
-
-RUN pip3 install --no-cache-dir awscli==1.18.178
-
-RUN apk add --no-cache jq groff less
-
-WORKDIR /aws
+FROM amazon/aws-cli:latest
+WORKDIR /root
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/sh -l
+mkdir /root/.aws
+#echo -n "[default]\nregion=us-east-1\noutput=json" > /root/config
+#echo -n "[default]\naws_access_key_id=$1\naws_secret_access_key=$2\naws_session_token=$3" > /root/credentials
+#echo "CONFIG"
+#cat /root/config
+#echo "CREDENTIALS"
+#cat /root/credentials
+#ls /root/
+#echo $PWD
+printf "[default]
+region=us-east-1
+output=json" | tee -a /root/.aws/config >/dev/null
+
+
+printf "[default]
+aws_access_key_id=$1
+aws_secret_access_key=$2
+aws_session_token=$3" | tee -a /root/.aws/credentials >/dev/null
+echo "my file /root/.aws/credentials:"
+cat /root/.aws/credentials
 aws --version
-echo "Hello $1"
+aws s3 ls
+#echo "Hello $1 $2 $3"
 #time=$(date)
 #echo "::set-output name=time::$time"
+
